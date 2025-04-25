dc07e8ef740bd490301db04dcbbe8e77e4be6405
Rosarioroderiga <rosario.roderiga@gmail.com>
2025-04-25 23:22:16 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { Configuration, OpenAIApi } = require('openai');
+const { sendConversationsEmail } = require('./dailyEmailSender');
 
 const app = express();
 const port = process.env.PORT || 3000;
